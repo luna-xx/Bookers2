9d092c57b3c2763bbe3ac191c57060e1be3b40b5
ryokatakahashi <ryonryon.xxxx@gmail.com>
2023-09-22 13:38:38 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'homes/about'
+  
+  resources :users, only: [:show, :edit]
+  
+  resources :books, only: [:new, :index, :show, :edit]
+  
+  root to: "homes#top"
+  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
